7c0c1998ada63df6a948d3d0a5b0856ac21a91ec
anyamiletic <anya.miletic@gmail.com>
2018-03-20 21:44:39 +0100

    simple crawler

--- download_webpage.py
+++ download_webpage.py
@@ -1,7 +1,9 @@
+import re
+
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
-def download(url, user_agent = wswp, num_retries=2):
+def download(url, user_agent = 'wswp', num_retries=2, charset='utf-8'):
 	print("Downloading: %s" % (url))
 	request = urllib.request.Request(url)
 
@@ -9,7 +11,13 @@ def download(url, user_agent = wswp, num_retries=2):
 	request.add_header('User-agent', user_agent)
 
 	try:
-		html = urllib.request.urlopen(url).read()
+		resp = urllib.request.urlopen(url)
+		cs = resp.headers.get_content_charset()
+
+		if not cs:
+			cs = charset
+
+		html = resp.read().decode(cs)
 	except (URLError, HTTPError,ContentTooShortError) as e:
 		print("Download error: %s" % (e.reason))
 		html = None
@@ -19,9 +27,21 @@ def download(url, user_agent = wswp, num_retries=2):
 
 	return html
 
+def crawl_sitemap(url):
+	#first we download the sitemap
+	sitemap = download(url)
+
+	#extract the links
+	links = re.findall('<loc>(.*?)</loc>', sitemap)
+
+	#enter each of the links
+	for link in links:
+		html = download(link)
+		print("entering: %s" % (link))
+
 def main():
 	url = input("Unesite url: ")
-	print(download(url))
+	crawl_sitemap(url)
 
 if __name__ == "__main__":
 	main()
